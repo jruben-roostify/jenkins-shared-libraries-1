533e42e4c4435c0341e4877181fd808156c04619
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-23 11:37:45 -0700

    Update sonarScanner.groovy

--- vars/sonarScanner.groovy
+++ vars/sonarScanner.groovy
@@ -7,9 +7,9 @@ def sonarRunner(String buildPath){
     sh "./gradlew -b "+buildPath+" sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD}"
   }else if (env.CHANGE_ID){
     echo 'This is a PR build. Running sonnar preview analysis'
-    sh "./gradlew -b "+buildPath+" sonar -Dsonar.github.pullRequest=${env.CHANGE_ID} -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.analysis.mode=preview -Dsonar.github.oauth=68eb8d979fe364931397bf39faa671159e5926ec -Dsonar.github.repository=${env.CUSTOM_SONAR_REPO_NAME} -i"
+    sh "./gradlew -b "+buildPath+" sonar -Dsonar.github.pullRequest=${env.CHANGE_ID} -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.analysis.mode=preview -Dsonar.github.oauth=${GITHUB_OAUTH_TOKEN} -Dsonar.github.repository=${env.CUSTOM_SONAR_REPO_NAME} -i"
   }else{
-    echo 'This is a branch build.'
+    echo 'This is a branch build. Running full branch analysis'
     sh "./gradlew -b "+buildPath+" sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.github.repository="+getRepoName()+" -Dsonar.branch=${env.GIT_BRANCH} -i"
   }
 }
